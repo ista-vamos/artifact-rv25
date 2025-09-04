35bb03ccce551b6b5cac3be62d229bda4e8b3076
Marek Chalupa <mchqwerty@gmail.com>
2025-09-04 08:32:56 +0000

    openssl: update run.py

--- experiments/openssl/run.py
+++ experiments/openssl/run.py
@@ -14,9 +14,8 @@ import signal
 import argparse
 
 bindir = f"{dirname(realpath(__file__))}/"
-mpt_binary = join(bindir, "mpt/monitor")
-rvhyper_dir = join(bindir, "rvhyper/")
 hnl_dir = join(bindir, ".")
+
 
 def errlog(*args):
     with open(join(dirname(__file__), "log.txt"), "a") as logf:
@@ -24,77 +23,22 @@ def errlog(*args):
             print(a, file=logf)
 
 def run_one(arg):
-    traces_num, trace_len, bits, args = arg
-    traces_dir = join(bindir, f'traces/traces-{bits}b')
+    traces_num, monitors, bits, args = arg
+    traces_dir = args.traces_dir
+    if traces_dir is None:
+        traces_dir = join(bindir, f'traces/traces-{bits}b')
 
     # get the list of files
-    if args.one_trace:
-        files = ["1.tr"] * traces_num
-    else:
-        files = [fl for fl in listdir(traces_dir) if fl.endswith(".tr")]
+    files = [fl for fl in listdir(traces_dir) if fl.endswith(".tr")][:traces_num]
 
     results = []
 
-    monitors = args.monitors
-
-    if "mpt" in monitors:
-        results.append(run_mpt(arg, traces_dir, files))
-    if "rvhyper" in monitors:
-        results.append(run_rvhyper(arg, traces_dir, files))
     if "ehl" in monitors:
         results.append(run_hnl(arg, traces_dir, files, "ehl"))
     if "shl-eq" in monitors:
         results.append(run_hnl(arg, traces_dir, files, "shl-eq"))
 
     return results
-
-
-def run_rvhyper(arg, traces_dir, files, rvh_args=None):
-    traces_num, trace_len, bits, args = arg
-    rvh = join(rvhyper_dir, "build/release/rvhyper")
-    assert access(rvh, X_OK), f"Cannon find rvhyper binary, assumed is {rvh}"
-    cmd = ["/bin/time", "-f", '%Uuser %Ssystem %eelapsed %PCPU (%Xavgtext+%Davgdata %Mmaxresident)k', rvh]
-    if rvh_args:
-        cmd += rvh_args
-    cmd += ["-S", f"{traces_dir}/od-{bits}b.hltl"] + files
-    # print("> ", " ".join(cmd))
-
-    p = Popen(cmd, stderr=PIPE, stdout=PIPE, cwd=traces_dir, preexec_fn=os.setsid)
-    try:
-        out, err = p.communicate(timeout=args.timeout)
-        if p.returncode != 0:
-            errlog(env, p, out, err)
-    except TimeoutExpired:
-        os.killpg(os.getpgid(p.pid), signal.SIGTERM)
-        out, err = p.communicate(timeout=10)
-
-    assert err is not None, cmd
-
-    cpu_time=None
-    wall_time=None
-    mem=None
-
-    if p.returncode == 0:
-        for line in err.splitlines():
-            if b"elapsed" in line:
-                parts = line.split()
-                assert b"user" in parts[0]
-                assert b"elapsed" in parts[2]
-                assert b"maxresident" in parts[5]
-
-                try:
-                    cpu_time = float(parts[0][:-4])
-                    wall_time = float(parts[2][:-7])
-                    mem = int(parts[5][:-13])/1024.0
-                except ValueError as e:
-                    print(err, file=sys.stderr)
-                    raise e
-    else:
-        errlog("Faield running RVHyper:", out, err)
-
-
-    return (f"rvhyper", traces_dir, traces_num, trace_len, bits, cpu_time, wall_time, mem, p.returncode)
-
 
 
 def run_hnl(arg, traces_dir, files, ty):
@@ -150,52 +94,9 @@ def run_hnl(arg, traces_dir, files, ty):
 
     return (f"{ty}", traces_dir, traces_num, trace_len, bits, verdict, instances, atoms, reused_mons, reused_verdicts, cpu_time, wall_time, mem, p.returncode)
 
-def run_mpt(arg, traces_dir, files):
-    traces_num, trace_len, bits, args = arg
-    cmd = ["/bin/time", "-f", '%Uuser %Ssystem %eelapsed %PCPU (%Xavgtext+%Davgdata %Mmaxresident)k',
-           mpt_binary]
-    cmd += files
-    p = Popen(cmd, stderr=PIPE, stdout=PIPE, cwd=traces_dir, preexec_fn=os.setsid)
-    try:
-        out, err = p.communicate(timeout=args.timeout)
-    except TimeoutExpired:
-        os.killpg(os.getpgid(p.pid), signal.SIGTERM) 
-        out, err = p.communicate(timeout=10)
-    assert err is not None, cmd
-
-
-    # Max workbag size: 7391
-    #Traces #: 500
-    #1.73user 0.03system 0:01.76elapsed 99%CPU (0avgtext+0avgdata 137604maxresident)k
-    #0inputs+0outputs (0major+34642minor)pagefaults 0swaps
-    wbg_size=None
-    cpu_time=None
-    wall_time=None
-    mem=None
-    if p.returncode in (0, 1):
-        for line in out.splitlines():
-            line = line.strip()
-            if line.startswith(b"Max workbag"):
-                wbg_size = int(line.split()[3])
-
-        for line in err.splitlines():
-            if b"elapsed" in line:
-                parts = line.split()
-                assert b"user" in parts[0]
-                assert b"elapsed" in parts[2]
-                assert b"maxresident" in parts[5]
-                cpu_time = float(parts[0][:-4])
-                wall_time = float(parts[2][:-7])
-                mem = int(parts[5][:-13])/1024.0
-    else:
-        errlog("Faield running MPT monitor:", out, err)
-
-    return ("mpt", traces_dir, traces_num, trace_len, bits, wbg_size, cpu_time, wall_time, mem, p.returncode)
 
 mon2bits = {
-        #'mpt' : 64,
         'shl-eq' : 64,
-        #'rvhyper' : 1,
         'ehl' : 8,
 }
 
@@ -203,14 +104,12 @@ def get_params(args):
     for N in args.traces_nums:
         for M in args.monitors:
             for _ in range(0, args.trials):
-                yield N, M, mon2bits[M], args
+                yield N, [M], mon2bits[M], args
 
 def run(args):
     print(f"\033[1;34mRunning using {args.j or 'automatic # of'} workers, output file is {args.out}\n\033[0m", file=stderr)
     print("Traces numbers: ", args.traces_nums)
     print("Run each configuration", args.trials, "times")
-    if args.one_trace:
-        print("All traces are the same one trace")
 
     verbose = args.verbose
 
@@ -243,33 +142,23 @@ def parse_cmd():
     parser.add_argument("-j", metavar="PROC_NUM", action='store', type=int)
     parser.add_argument("--out", help="Name of the output file. Default is 'out.csv'", action='store', default="out.csv")
     parser.add_argument("--verbose", help="Print some extra messages", action='store_true', default=False)
-    #parser.add_argument("--traces-dir", help="Take traces from this dir. If the dir does not exists, generate traces to this dir", action='store')
+    parser.add_argument("--traces-dir", help="Take traces from this dir.", action='store')
 
-    parser.add_argument("--traces-lens", help="Comma-separated list of lenghts of traces", action='store',
-                        default=[5000, 10000, 15000, 20000])
     parser.add_argument("--traces-nums", help="Comma-separated list of numbers of traces", action='store',
                         default=[1000, 5000, 10000, 15000, 20000])
-    parser.add_argument("--bits", help="Comma-separated list of bits for the alphabet (not affecting mpt and shl monitors). Supported are any combination of 1, 2, 4, 8, 10.",
-                        action='store', default=[4,8,10])
     parser.add_argument("--trials", help="How many times repeat each run", action='store', type=int, default=10)
     parser.add_argument("--timeout", help="In seconds", action='store', type=int, default=300)
     parser.add_argument("--monitors", help="List of monitors: mpt, rvhyper, ehl, shl-eq", action='store',
-                        #default="ehl,shl-eq,rvhyper,mpt")
                         default="ehl,shl-eq")
 
-    parser.add_argument("--one-trace", help="Make all traces same", action='store_true', default=False)
     parser.add_argument("--traces-no-stuttering", help="Generate traces with no stuttering", action='store_true', default=False)
 
     args = parser.parse_args()
 
-    #if args.traces_dir:
-    #    args.traces_dir = abspath(args.traces_dir)
-    if isinstance(args.traces_lens, str):
-        args.traces_lens = list(map(int, args.traces_lens.split(",")))
+    if args.traces_dir:
+        args.traces_dir = abspath(args.traces_dir)
     if isinstance(args.traces_nums, str):
         args.traces_nums = list(map(int, args.traces_nums.split(",")))
-    if isinstance(args.bits, str):
-        args.bits = list(map(int, args.bits.split(",")))
 
     args.monitors = args.monitors.split(",")
 
